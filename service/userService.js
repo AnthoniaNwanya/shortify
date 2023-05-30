858bf5043c4fa32c6f1d406efcd2d90b285bb26e
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-05-30 13:01:33 +0100

    modify update/delete

--- service/userService.js
+++ service/userService.js
@@ -2,17 +2,17 @@ const UserSchema = require("../schema/UserSchema");
 const { NotFoundError, BadRequestError, ForbiddenError, ValidationError } = require("../middleware/Error");
 
 const signup = async (data) => {
-        const existingUser = await UserSchema.findOne({ "email": data.email });
-        if (existingUser) {
-            throw new ForbiddenError("User already exists");
-        } else {
-             const createUser = await UserSchema.create(data);
-              return createUser
-        }
+    const existingUser = await UserSchema.findOne({ "email": data.email });
+    if (existingUser) {
+        throw new ForbiddenError("User already exists");
+    } else {
+        const createUser = await UserSchema.create(data);
+        return createUser
+    }
 };
 
 const getUsers = async (data) => {
-    const users = await UserSchema.find({}, {password:0});
+    const users = await UserSchema.find({}, { password: 0 });
     if (!users) {
         throw new NotFoundError("Users not found");
     }
@@ -23,7 +23,7 @@ const getOne = async (email) => {
     if (!email) {
         throw new ValidationError("email is required")
     }
-    let foundUser = await UserSchema.findOne({ "email": email }, {password:0});
+    let foundUser = await UserSchema.findOne({ "email": email }, { password: 0 });
     if (!foundUser) {
         throw new NotFoundError("User not found")
     }
@@ -33,16 +33,14 @@ const getOne = async (email) => {
 const updateOne = async (id, userUpdate) => {
     const update = await UserSchema.findByIdAndUpdate(id, userUpdate, {
         new: true,
-    }, );
+    });
+
+    if (!update || id === undefined) {
+        throw new BadRequestError("Verify user or new update");
+    }
     update.updatedAt = new Date();
     await update.save();
 
-
-    if (id === undefined) {
-        throw new BadRequestError("User not found");
-    } else if (!update) {
-        throw new BadRequestError("Unable to update user");
-    }
 
     return update;
 };
@@ -50,7 +48,7 @@ const updateOne = async (id, userUpdate) => {
 const deleteOne = async (id) => {
     const deletedUser = await UserSchema.findByIdAndDelete(id);
     if (!deletedUser) {
-        throw new BadRequestError("Unable to delete user");
+        throw new BadRequestError("User does not exist");
     }
     return deletedUser
 
