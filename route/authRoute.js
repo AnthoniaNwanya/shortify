2c37a882279d86cac1d48bc7057f12a12a0ec88f
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-06-01 23:35:47 +0100

    set cookie

--- route/authRoute.js
+++ route/authRoute.js
@@ -3,7 +3,6 @@ const controller = require("../controller/userController");
 const router = express.Router();
 
 router.post("/signup",  controller.signup);
-// router.get("/signup", controller.signupView);
 router.post("/login", controller.login);
 
 module.exports = router;
