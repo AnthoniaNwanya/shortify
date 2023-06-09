3a72e6dcba9f170c9bc9e2aa128b26ffbe1f0eb8
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-06-09 03:19:44 +0100

    config

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const UserRoute = require("./route/userRoute");
 const UrlRoute = require("./route/urlRoute");
 const RedirectRoute = require("./route/redirectRoute");
 const ErrorHandler = require("./middleware/ErrorHandler");
-const {authenticateUser} = require("./middleware/authentication");
+const { authenticateUser } = require("./middleware/authentication");
 const { ForbiddenError } = require("./middleware/Error");
 const limitRate = require("./middleware/rateLimiter");
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
 
 const app = express();
 if (process.env.NODE_ENV === "development") {
- app.use(limitRate);
+  // app.use(limitRate);
 }
 app.use(
   session({
@@ -22,9 +26,10 @@ app.use(
     resave: false,
     saveUninitialized: true,
   })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  );
+  app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(express.static('public'));
 
@@ -32,15 +37,39 @@ app.set('views', path.join('views'))
 app.set('view engine', 'ejs');
 
 app.get('/api/signup', (req, res) => {
-  res.render('signup')
+  res.render('signup');
 })
 app.get('/api/login', (req, res) => {
-  res.render('login')
-})
-app.get('/api/shortify',  authenticateUser,
-(req, res) => {
-  res.render('home')
-})
+  res.render('login');
+});
+app.get('/api/shortify', authenticateUser, (req, res) => {
+  res.render('home', {
+    user: req.User
+  });
+});
+app.get('/api/dashboard', authenticateUser, (req, res) => {
+  res.render('dashboard', {
+    user: req.User
+  });
+});
+
+app.get('/logout', authenticateUser, (req, res) => {
+  res.clearCookie("token");
+  res.redirect('/api/login');
+});
+app.get('/api/user/update/:id', authenticateUser, (req, res) => {
+
+  res.render('settings', {
+    user: req.User
+
+  });
+});
+app.get('/api/user/delete/:id', authenticateUser, (req, res) => {
+  res.render('deactivate', {
+    user: req.User.id
+  });
+
+});
 
 app.use("/api", AuthRoute);
 app.use("/api/user", UserRoute);
--- public/login.js
+++ public/login.js
@@ -1,84 +1,21 @@
-// const form = document.querySelector("form"),
-// emailField = form.querySelector(".email-field"),
-// emailInput = emailField.querySelector(".email")
+const form = document.querySelector("form"),
+emailField = form.querySelector(".email-field"),
+emailInput = emailField.querySelector(".email")
 
-// // Email Validtion
-// function checkEmail() {
-// const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-// if (!emailInput.value.match(emailPattern)) {
-//     return emailField.classList.add("invalid");
-// }
-// emailField.classList.remove("invalid");
-// }
-// // Calling Funtion on Form Sumbit
-// form.addEventListener("submit", (e) => {
-// e.preventDefault();
-// checkEmail();
+function checkEmail() {
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+if (!emailInput.value.match(emailPattern)) {
+  return emailField.classList.add("invalid");
+}
+emailField.classList.remove("invalid");
+}
 
-// //calling function on key up
-// emailInput.addEventListener("keyup", checkEmail);
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+checkEmail();
+emailInput.addEventListener("keyup", checkEmail);
 
-// if (
-//     !emailField.classList.contains("invalid") 
-// ) {
-//     getToken()
-// }
-// });
-
-// function getToken() {
-//     var loginUrl = "http://localhost:8000/api/login"
-//     var xhr = new XMLHttpRequest();
-//     var userElement = document.getElementById('emailID');
-//     // var passwordElement = document.getElementById('password');
-//     var tokenElement = document.getElementById('token');
-//     var user = userElement.value;
-//     // var password = passwordElement.value;
-  
-//     xhr.open('POST', loginUrl, true);
-//     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-//     xhr.addEventListener('load', function() {
-//       var responseObject = JSON.parse(this.response);
-//       console.log(responseObject);
-//       if (responseObject.token) {
-//         // tokenElement.innerHTML = responseObject.token;
-//         window.localStorage.setItem('token', token);
-//         var url = "http://localhost:8000/api/shortify"
-//         var xhr = new XMLHttpRequest();
-//         // var tokenElement = document.getElementById('token');
-//         // var resultElement = document.getElementById('result');
-//         xhr.open('GET', url, true);
-//         xhr.setRequestHeader("Authorization", "JWT " + window.localStorage.getItem('token'));
-//         window.location.href = "http://localhost:8000/api/shortify"
-//         console.log(token)
-//       } else {
-//         tokenElement.innerHTML = "No token received"
-//       }
-//     });
-  
-//     var sendObject = JSON.stringify({email: user});
-  
-//     console.log('going to send', sendObject);
-  
-//     xhr.send(sendObject);
-//   }
- 
-
-  // function getSecret() {
-
-  //   var url = "http://localhost:8000/api/shortify"
-  //   var xhr = new XMLHttpRequest();
-  //   var tokenElement = document.getElementById('token');
-  //   var resultElement = document.getElementById('result');
-  //   xhr.open('GET', url, true);
-  //   xhr.setRequestHeader("Authorization", "JWT " + window.localStorage.getItem(tokenElement));
-  //   window.location = "http://localhost:8000/api/shortify"
-  //   // xhr.addEventListener('load', function() {
-  //   //   var responseObject = JSON.stringify(this.response);
-  //   //   console.log(responseObject);
-  //   //   resultElement.innerHTML = this.responseText;
-  //   // });
-  
-  //   // xhr.send(null);
-  // }
- 
-  
+if (!emailField.classList.contains("invalid")) {
+  document.querySelector("form").submit();
+}
+});
