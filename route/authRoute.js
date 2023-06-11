89b72973037af878d5ee0b4857e86f1650d02513
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-06-11 02:13:42 +0100

    use request validation

--- route/authRoute.js
+++ route/authRoute.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const controller = require("../controller/userController");
 const router = express.Router();
-const  {validateRequest} = require("../middleware/validateReq");
-const  {userSignUpValidation} = require("../validation/userSchema");
 
-router.post("/signup", validateRequest(userSignUpValidation), controller.signup);
+router.post("/signup", controller.signup);
 router.post("/login", controller.login);
 
 module.exports = router;
