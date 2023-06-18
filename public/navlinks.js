04f4681aab73efaafd9648482995e7a33749dabe
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-06-18 01:19:06 +0100

    make regex pass verify all characters

--- public/navlinks.js
+++ public/navlinks.js
@@ -1,3 +1,40 @@
+const form = document.querySelector(".form"),
+emailField = form.querySelector(".email-field"),
+emailInput = emailField.querySelector(".email"),
+passField = form.querySelector(".create-password"),
+passInput = passField.querySelector(".password")
+
+// Email Validtion
+function checkEmail() {
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emailPattern)) {
+        return emailField.classList.add("invalid"); 
+    }
+    emailField.classList.remove("invalid"); 
+    }
+    
+    function createPass() {
+    const passPattern =
+    /^(?=.*[-\#\$\.\?\%\&\@\!\+\=\\*])(?=.*[a-zA-Z])(?=.*\d).{8,}$/;    
+    
+    if (!passInput.value.match(passPattern)) {
+        return passField.classList.add("invalid"); 
+    }
+    passField.classList.remove("invalid"); 
+    }
+    form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkEmail();
+    createPass();
+    emailInput.addEventListener("keyup", checkEmail);
+    passInput.addEventListener("keyup", createPass);
+    
+    if (
+        !emailField.classList.contains("invalid") &&
+        !passField.classList.contains("invalid")
+    ) {
+    document.querySelector(".form").submit();			}
+    });
 
 const navBar = document.querySelector("nav"),
 menuBtns = document.querySelectorAll(".menu-icon");
